fb3f41b895055ffa1ff9c613502ec37d28eac81e
Gabe Sequeira-Bacher <gabesequeirabacher@gmail.com>
2020-07-12 19:06:01 -0400

    solved return story

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-// import L from 'leaflet'
+// import L from "leaflet";
 import VTMap from "./VTMap";
 import PointChecker from "./Components/PointGenerator";
-import './App.css';
+import "./App.css";
 //import ModalConductor from './ModalConductor'
-import Modal from './Components/Modal';
-
+import { Modal, selectedCounty } from "./Components/Modal";
 
 class App extends Component {
   constructor(props) {
@@ -14,160 +13,225 @@ class App extends Component {
     this.state = {
       gameStarted: false,
       coords: [43.9, -72.5],
+      origCoords: [],
       zoom: 7,
       latStatus: "",
       longStatus: "",
       county: "",
       town: "",
-      currentModal: '',
-      guessModalDisplay: 'none'
+      currentModal: "",
+      guessModalDisplay: "none",
+      gameStatus: "",
+      startButtonDisplay: "",
+      restartButtonDisplay: "none"
     };
+  }
+
+  restartGame = (evt) => {
+    this.StartGame(evt);
   }
 
   //start function gets everything you need for the game and stores it in state.
   StartGame = (evt) => {
-    this.setState({ gameStarted: true })
+    this.setState({ 
+      gameStarted: true,
+      startButtonDisplay: 'none'
+    });
     evt.preventDefault();
     let theCoordinate = PointChecker();
-    console.log(theCoordinate[0])
-    fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${theCoordinate[0]}&lon=${theCoordinate[1]}`)
-    .then(data => data.json())
-    .then(jsonObj => {
-      console.log(jsonObj)
-      let town;
-      town = jsonObj.address.city || jsonObj.address.town || jsonObj.address.village || jsonObj.address.hamlet
-      this.setState({
-        mainMap: {
-          // lat: flippedPoint[0],
-          // lon: flippedPoint[1],
+    fetch(
+      `https://nominatim.openstreetmap.org/reverse?format=json&lat=${theCoordinate[0]}&lon=${theCoordinate[1]}`
+    )
+      .then((data) => data.json())
+      .then((jsonObj) => {
+        console.log(jsonObj);
+        let town;
+        town =
+          jsonObj.address.city ||
+          jsonObj.address.town ||
+          jsonObj.address.village ||
+          jsonObj.address.hamlet;
+        this.setState({
+          mainMap: {
+            // lat: flippedPoint[0],
+            // lon: flippedPoint[1],
+          },
           zoom: 18,
-        },
-        // mapCoords: [flippedPoint],
-        county: jsonObj.address.county,
-        town: town,
-        score: 100,
-        gameStarted: true
-      })
-      console.log(this.state.gameStarted)
-    })
+          coords: [theCoordinate[0], theCoordinate[1]],
+          origCoords: [theCoordinate[0], theCoordinate[1]],
+          // mapCoords: [flippedPoint],
+          county: jsonObj.address.county,
+          town: town,
+          points: 100,
+          gameStarted: true,
+          latStatus: "?",
+          longStatus: "?",
+          countyStatus: "?",
+          townStatus: "?",
+          startButtonDisplay: 'none'
+        });
+      });
     // move map to random coordinates
+  };
 
-    let result = PointChecker();
-    console.log(result);
+  guessHandler = (evt) => {
+    evt.preventDefault();
+    this.setState((prevState) => {
+      //return { guessModal: !prevState.guessModal};
+      return { guessModalDisplay: "" };
+    });
+  };
+
+  handleClose = (evt) => {
+    evt.preventDefault();
     this.setState(() => {
       return {
-        coords: [result[0], result[1]],
-        zoom: 18,
-        latStatus: "?",
-        longStatus: "?",
-        countyStatus: "?",
-        townStatus: "?",
-        points: 100
+        guessModalDisplay: "none",
       };
     });
   };
 
-componentDidMount() {
-  console.log(this.state.guessModalDisplay)
-}
-
-guessHandler = (evt) => {
-  evt.preventDefault();
-  this.setState((prevState) => {
-    //return { guessModal: !prevState.guessModal};
-    return { guessModalDisplay: '' }
-  });
-};
-
-hideModal = (evt) => {
-  evt.preventDefault();
-  this.setState(() => {
-    return { currentModal: ''};
-  });
-};
-
   giveUp = (evt) => {
-    evt.preventDefault()
-
+    evt.preventDefault();
+    // let lat = this.state.coords[0];
+    // let lon = this.state.coords[1];
     this.setState(() => {
       return {
-        latStatus: (this.state.coords[0]).toFixed(4),
-        longStatus: (this.state.coords[1]).toFixed(4),
+        coords: [43.9, -72.5],
+        zoom: 7,
+        latStatus: this.state.coords[0].toFixed(4),
+        longStatus: this.state.coords[1].toFixed(4),
         countyStatus: this.state.county,
         townStatus: this.state.town,
-        points: -100
+        points: -100,
       };
     });
-}
+  };
 
-directional = () => {
+  directional = () => {
     this.setState(() => {
       let newPoints = this.state.points - 1;
       return {
-        points: newPoints
-      }
-  })
-}
+        points: newPoints,
+      };
+    });
+  };
 
-moveNorth = (evt) => {
-  evt.preventDefault();
-  this.setState(() => {
-    let newLat = this.state.coords[0] + .002;
-    return {
-    coords: [newLat, this.state.coords[1]],
-  }
-})
-  this.directional()
-}
+  moveNorth = (evt) => {
+    evt.preventDefault();
+    this.directional();
+    this.setState(() => {
+      let newLat = this.state.coords[0] + 0.002;
+      return {
+        coords: [newLat, this.state.coords[1]],
+      };
+    });
+  };
 
   moveSouth = (evt) => {
     evt.preventDefault();
+    this.directional();
     this.setState(() => {
-      let newLat = this.state.coords[0] - .002;
+      let newLat = this.state.coords[0] - 0.002;
       return {
-      coords: [newLat, this.state.coords[1]],
-    }
-  })
-    this.directional()
+        coords: [newLat, this.state.coords[1]],
+      };
+    });
   };
 
   moveEast = (evt) => {
     evt.preventDefault();
+    this.directional();
     this.setState(() => {
-      let newLong = this.state.coords[1] + .003;
+      let newLong = this.state.coords[1] + 0.003;
       return {
-      coords: [this.state.coords[0], newLong],
-    }
-  })
-  this.directional()
+        coords: [this.state.coords[0], newLong],
+      };
+    });
   };
 
   moveWest = (evt) => {
     evt.preventDefault();
+    this.directional();
     this.setState(() => {
-      let newLong = this.state.coords[1] - .003;
+      let newLong = this.state.coords[1] - 0.003;
       return {
-      coords: [this.state.coords[0], newLong],
-    }
-  })
-  this.directional()
+        coords: [this.state.coords[0], newLong],
+      };
+    });
   };
+
+  guessSubmit = () => {
+    console.log("selected county: ");
+    console.log(selectedCounty);
+    console.log("this.state.county:");
+    console.log(this.state.county);
+    if (selectedCounty === this.state.county) {
+      this.setState(() => {
+        return {
+          gameStarted: false,
+          guessModalDisplay: "none",
+          restartButtonDisplay: "",
+          latStatus: this.state.coords[0].toFixed(4),
+          longStatus: this.state.coords[1].toFixed(4),
+          countyStatus: this.state.county,
+          townStatus: this.state.town,
+          gameStatus: "Correct!",
+        };
+      });
+    } else {
+      this.setState(() => {
+        let newPoints = this.state.points - 10;
+        return {
+          points: newPoints,
+          gameStatus: "Sorry, try again!",
+        };
+      });
+    }
+
+    this.setState(() => {
+      return {
+        guessModalDisplay: "none",
+      };
+    });
+  };
+
+  return = (evt) => {
+    evt.preventDefault();
+    this.setState(() => {
+      return {
+        coords: this.state.origCoords
+      }
+    })
+  }
 
   render() {
     return (
-      <div id='main'>
+      <div id="main">
         {/* <ModalConductor close={this.hideModal} /> */}
-        <Modal guessModalDisplay={this.state.guessModalDisplay} />
+        <Modal
+          guessModalDisplay={this.state.guessModalDisplay}
+          handleClose={this.handleClose}
+          guessedCounty={this.props.guessedCounty}
+          guessSubmit={this.guessSubmit}
+          handleChange={this.handleChange}
+        />
         {/* GAME MAP  */}
-        <VTMap z-index='0' coords={this.state.coords} zoom={this.state.zoom} zoomControl={false} scrollWheelZoom={false} touchZoom={false} doubleClickZoom={false}
-        guessModal={this.state.guessModal}
-        >
-        </VTMap>
+        <VTMap
+          z-index="0"
+          coords={this.state.coords}
+          zoom={this.state.zoom}
+          zoomControl={false}
+          scrollWheelZoom={false}
+          touchZoom={false}
+          doubleClickZoom={false}
+          guessModal={this.state.guessModal}
+        ></VTMap>
         {/* START BUTTON  */}
         <button
           id="start"
           onClick={this.StartGame}
-          style={{ display: this.state.gameStarted ? "none" : "inline" }}
+          style={{ display: this.state.startButtonDisplay }}
         >
           Start Game
         </button>
@@ -197,15 +261,15 @@ moveNorth = (evt) => {
           <div id="count_field">County: {this.state.countyStatus}</div>
           <div id="town_field">Town: {this.state.townStatus}</div>
           <div id="points_field">Points: {this.state.points}</div>
+          <div id="game_status">Game status: {this.state.gameStatus}</div>
         </div>
 
         {/* DIRECTION BUTTONS  */}
         <div
           id="button_container"
-          style={{ border: "1px solid black", width: '30vw', height: '30vw'}}
         >
           <button
-            className='button'
+            className="button"
             id="northBtn"
             onClick={this.moveNorth}
             disabled={this.state.gameStarted ? false : true}
@@ -213,7 +277,7 @@ moveNorth = (evt) => {
             N
           </button>
           <button
-            className='button'
+            className="button"
             id="southBtn"
             onClick={this.moveSouth}
             disabled={this.state.gameStarted ? false : true}
@@ -221,7 +285,7 @@ moveNorth = (evt) => {
             S
           </button>
           <button
-            className='button'
+            className="button"
             id="eastBtn"
             onClick={this.moveEast}
             disabled={this.state.gameStarted ? false : true}
@@ -229,12 +293,28 @@ moveNorth = (evt) => {
             E
           </button>
           <button
-            className='button'
+            className="button"
             id="westBtn"
             onClick={this.moveWest}
             disabled={this.state.gameStarted ? false : true}
           >
             W
+          </button>
+          {/* <button
+          className='button'
+          id='zoomInBtn'
+          onClick={this.zoomIn}>Zoom In</button> */}
+          <button
+            className='button'
+            id='returnButton'
+            onClick={this.return}
+            style={{display: this.state.gameStarted ? 'inline' : 'none'}}>Return</button>
+          <button 
+            id="restart_btn" 
+            onClick={this.restartGame}
+            style={{display: this.state.restartButtonDisplay}}
+            >
+              Play again?
           </button>
         </div>
       </div>
--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,34 +1,58 @@
 import React, { Component } from "react"
 
+
+let selectedCounty;
+
 class Modal extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            guessedCounty: "",
+        }
+    }
+    
+
+    handleChange = (event) => {
+        event.preventDefault();
+        console.log(event.target.value)
+        // this.setState(() => {
+        //     return {
+        //     guessedCounty: event.target.value
+        //     }
+        // })
+        selectedCounty = `${event.target.value} County`;
+      }
+
     render() {
         return(
-            <div id="guess_modal_background" style={{ display: this.props.guessModalDisplay}}>
+            <div id="guess_modal_background" style={{ display: this.props.guessModalDisplay }}>
                 <div id="guess_form_container">
-                    <input id="county_input" type="text" list="counties" onChange={this._onChange} />
-                    <datalist id="counties" >
-                            <option value="Addison" />
-                            <option value="Bennington" />
-                            <option value="Caledonia" />
-                            <option value="Chittenden" />
-                            <option value="Essex" />
-                            <option value="Franklin" />
-                            <option value="Grand Isle" />
-                            <option value="Orange" />
-                            <option value="Orleans" />
-                            <option value="Rutland" />
-                            <option value="Washington" />
-                            <option value="Windham" />
-                            <option value="Windsor" />
-                        </datalist>
+                    <h3 id="county_title" className="guess_element">County:</h3>
+                    
+                    <select id="counties"  onChange={this.handleChange}>
+                            <option value="Addison" >Addison</option>
+                            <option value="Bennington" >Bennington</option>
+                            <option value="Caledonia" >Caledonia</option>
+                            <option value="Chittenden" >Chittenden</option>
+                            <option value="Essex">Essex</option>
+                            <option value="Franklin" >Franklin</option>
+                            <option value="Grand Isle" >Grand Isle</option>
+                            <option value="Lamoille" >Lamoille</option>
+                            <option value="Orange" >Orange</option>
+                            <option value="Orleans" >Orleans</option>
+                            <option value="Rutland">Rutland</option>
+                            <option value="Washington">Washington</option>
+                            <option value="Windham">Windham</option>
+                            <option value="Windsor">Windsor</option>
+                        </select>
 
-                        <button id="guess_btn">Guess</button>
+                        <button id="guess_btn" className="guess_element" onClick={this.props.guessSubmit}>Guess</button>
+                        <div id="guess_close_button" onClick={this.props.handleClose}>+</div>
                 </div>
             </div>
-
-
         )
     }
 }
 
-export default Modal
+export { Modal, selectedCounty };
